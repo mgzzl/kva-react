cd999879bfe7cf868449a199fc4912f9063d7432
mg06201 <max.geissler@stud.th-deg.de>
2024-06-25 14:02:08 +0200

    add login

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import Login from './login';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   // document.getElementById('root')
 );
---
+++ src/login.tsx
@@ -0,0 +1,56 @@
+// src/login.tsx
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Navigate, Routes, Route } from 'react-router-dom';
+import App from './App';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // Add your login logic here
+        if (username === 'kg-kva' && password === 'Rolex420!') {
+            setIsLoggedIn(true);
+            return <Navigate to="/" replace={true} />;
+        } else {
+            console.log("Please provide a valid username and password")
+        }
+    };
+
+    return (
+        <div className="container">
+            {isLoggedIn ? (
+            <Router>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                </Routes>
+            </Router>
+            ) : (
+                <>
+                    <h1>Login</h1>
+                    <form onSubmit={handleLogin}>
+                        <div className="form-group row">
+                            <label className="col-sm-2 col-form-label">Username</label>
+                            <div className="col-sm-10">
+                                <input type="text" className="form-control" id="username" placeholder="username" value={username} onChange={(event) => setUsername(event.target.value)} />
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label className="col-sm-2 col-form-label">Password</label>
+                            <div className="col-sm-10">
+                                <input type="password" className="form-control" id="password" placeholder="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                            </div>
+                        </div>
+                        <br />
+                        <button type="submit">Login</button>
+                    </form>
+
+                </>
+            )}
+        </div>
+    );
+}
+
+
+export default Login;
